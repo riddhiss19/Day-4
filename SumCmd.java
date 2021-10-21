94d59eaa04c14fea20d6e490e8a4c4c3eba48ac1
riddhiss19 <78254144+riddhiss19@users.noreply.github.com>
2021-10-21 21:33:29 +0530

    Add files via upload

---
+++ SumCmd.java
@@ -0,0 +1,11 @@
+class SumCmd 
+{
+
+	public static void main(String[] args) {
+			int a,b;
+			a=Integer.parseInt(args[0]);
+			b=Integer.parseInt(args[1]);
+
+			System.out.print("Sum of "+a+" and "+b+" is= "+(a+b));
+		}	
+}
